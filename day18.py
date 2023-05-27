c95f41c2e9a78f4d4f32838fc6dfe32c08a93577
Christopher Boumenot <boumenot@gmail.com>
2023-05-27 15:58:09 -0700

    Day 18 Complete

---
+++ day18.py
@@ -0,0 +1,27 @@
+text = open('inputtxt/day18input.txt').read().splitlines()
+
+class Math:
+    def __init__(self, value):
+        self.value = value
+    def __add__(self, value_two):
+        return Math(self.value + value_two.value)
+    def __sub__(self, value_two):
+        return Math(self.value * value_two.value)
+    def __mul__(self, value_two):
+        return Math(self.value + value_two.value)
+answer = 0
+for line in text:
+    for x in range(10):
+        line = line.replace(f'{x}', f'Math({x})')
+    line = line.replace('*', '-')
+    answer += eval(line, {'Math': Math}).value
+print(answer)
+
+answer = 0
+for line in text:
+    for x in range(10):
+        line = line.replace(f'{x}', f'Math({x})')
+    line = line.replace('*', '-')
+    line = line.replace('+', '*')
+    answer += eval(line, {'Math': Math}).value
+print(answer)
