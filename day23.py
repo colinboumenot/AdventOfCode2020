18e0eaa612290649f080887319f36279ba00c66a
Christopher Boumenot <colin.boumenot@yahoo.com>
2023-06-08 19:56:30 -0700

    Day 23 Edited

--- day23.py
+++ day23.py
@@ -1,3 +1,19 @@
-text = open('inputtxt/day23input.txt').read().strip().splitlines()
+text = str(open('inputtxt/day23input.txt').read())
+text = [int(x) for x in text]
+neighbor = [None for _ in range(11)]
+
+from copy import deepcopy
+moveable_text = deepcopy(text)
+moveable_neighbor = deepcopy(neighbor)
+
+for x in range(len(moveable_text)):
+    moveable_neighbor[moveable_text[x]] = moveable_text[(x+1) % len(moveable_text)]
+count = 0
+current = moveable_text[0]
+for _ in range(100):
+    count += 1
+    selected = moveable_neighbor[current]
+    moveable_neighbor[current] = moveable_neighbor[moveable_neighbor[moveable_neighbor[selected]]]
+
 
 
