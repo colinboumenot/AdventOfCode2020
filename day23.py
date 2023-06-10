afad584c5480bf9c1c110c33246f258f56ef1181
Christopher Boumenot <colin.boumenot@yahoo.com>
2023-06-10 16:45:54 -0700

    Day 23 Part 1 Complete

--- day23.py
+++ day23.py
@@ -5,7 +5,6 @@ neighbor = [None for _ in range(11)]
 from copy import deepcopy
 moveable_text = deepcopy(text)
 moveable_neighbor = deepcopy(neighbor)
-
 for x in range(len(moveable_text)):
     moveable_neighbor[moveable_text[x]] = moveable_text[(x+1) % len(moveable_text)]
 count = 0
@@ -14,6 +13,22 @@ for _ in range(100):
     count += 1
     selected = moveable_neighbor[current]
     moveable_neighbor[current] = moveable_neighbor[moveable_neighbor[moveable_neighbor[selected]]]
-
-
+    if current == 1:
+        destination = 9
+    else:
+        destination = current - 1
+    while destination in [selected, moveable_neighbor[selected], moveable_neighbor[moveable_neighbor[selected]]]:
+        if destination == 1:
+            destination = 9
+        else:
+            destination -= 1
+    moveable_neighbor[moveable_neighbor[moveable_neighbor[selected]]] = moveable_neighbor[destination]
+    moveable_neighbor[destination] = selected
+    current = moveable_neighbor[current]
+answer = []
+x = moveable_neighbor[1]
+while x != 1:
+    answer.append(x)
+    x = moveable_neighbor[x]
+print(''.join([str(x) for x in answer]))
 
