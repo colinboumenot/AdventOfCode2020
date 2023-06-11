843a5523099430057b2156aceb99aff1095f0191
Christopher Boumenot <colin.boumenot@yahoo.com>
2023-06-11 13:22:36 -0700

    Day 23 Complete

--- day23.py
+++ day23.py
@@ -32,3 +32,33 @@ while x != 1:
     x = moveable_neighbor[x]
 print(''.join([str(x) for x in answer]))
 
+neighbor = [None for _ in range(1000001)]
+moveable_text = deepcopy(text)
+moveable_neighbor = deepcopy(neighbor)
+
+for x in range(len(moveable_text)):
+    moveable_neighbor[moveable_text[x]] = moveable_text[(x+1) % len(moveable_text)]
+moveable_neighbor[moveable_text[-1]] = 10
+for x in range(10, 1000001):
+    moveable_neighbor[x] = x + 1
+moveable_neighbor[-1] = moveable_text[0]
+
+count = 0
+current = moveable_text[0]
+for _ in range(10000000):
+    count += 1
+    selected = moveable_neighbor[current]
+    moveable_neighbor[current] = moveable_neighbor[moveable_neighbor[moveable_neighbor[selected]]]
+    if current == 1:
+        destination = 1000000
+    else:
+        destination = current - 1
+    while destination in [selected, moveable_neighbor[selected], moveable_neighbor[moveable_neighbor[selected]]]:
+        if destination == 1:
+            destination = 1000000
+        else:
+            destination -= 1
+    moveable_neighbor[moveable_neighbor[moveable_neighbor[selected]]] = moveable_neighbor[destination]
+    moveable_neighbor[destination] = selected
+    current = moveable_neighbor[current]
+print(moveable_neighbor[1] * moveable_neighbor[moveable_neighbor[1]])
