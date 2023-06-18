2c1e81e475fa099c4f0b7662ac5c031b0e2927b0
Christopher Boumenot <colin.boumenot@yahoo.com>
2023-06-17 18:55:04 -0700

    Day 24 Completed

--- day24.py
+++ day24.py
@@ -35,3 +35,23 @@ for line in text:
     else:
         black_tiles.add((x, y , z))
 print(len(black_tiles))
+
+moves = [(1, 0, -1), (-1, 0, 1), (1, -1, 0), (-1, 1, 0), (0, 1, -1), (0, -1, 1)]
+for _ in range(100):
+    final_black = set()
+    checked = set()
+    for (x, y, z) in black_tiles:
+        checked.add((x, y, z))
+        for (dx, dy, dz) in moves:
+            checked.add((x + dx, y + dy, z + dz))
+    for (x, y, z) in checked:
+        answer = 0
+        for (dx, dy, dz) in moves:
+            if (x + dx, y + dy, z + dz) in black_tiles:
+                answer += 1
+        if (x, y, z) in black_tiles and (answer == 1 or answer == 2):
+            final_black.add((x, y, z))
+        if (x, y, z) not in black_tiles and answer == 2:
+            final_black.add((x, y, z))
+    black_tiles = final_black
+print(len(black_tiles))
